783af627337be7e4d98822abb8bfe43390c51169
Bkgediya <bhavdipgediya099@gmail.com>
2024-09-23 20:47:49 +0530

    the http server

--- index.js
+++ index.js
@@ -1,2 +1,33 @@
 const http = require('http')
 console.log(http)
+
+
+const server = http.createServer((req, res) => {
+  if (req.url === '/') {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Home page\n');
+  }
+  
+  if (req.url === '/about') {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('About page\n');
+  }
+});
+
+server.listen(3000, () => {
+  console.log('Server running at http://127.0.0.1:3000/');
+});
+
+// const express = require('express');
+// const app = express();
+// const port = 3000;
+
+// app.get('/', (req, res) => {
+//   res.send('Hello World!');
+// });
+
+// app.listen(port, () => {
+//   console.log(`Example app listening at http://localhost:${port}`);
+// });
